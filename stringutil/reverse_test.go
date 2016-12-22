76bdc2cd094ceb0249bf824ed50b33b1f21f57cd
Nilav Khatiwada <mysteriousguyn@gmail.com>
2016-12-22 13:32:14 +0545

    pushing go testing code

---
+++ stringutil/reverse_test.go
@@ -0,0 +1,25 @@
+package stringutil_test
+
+import (
+	"ginkgoreverse/stringutil"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ginkgoreverse", func() {
+
+	var value string
+	Context("Here we reverse the string", func() {
+
+		BeforeEach(func() {
+			value = stringutil.Reverse("hello")
+		})
+
+		It("Successfully reveresed", func() {
+			Expect(value).To(Equal("abey"))
+		})
+
+	})
+
+})
